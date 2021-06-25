1a679de44a37b8db3b89c916a65d4bbe85127ebb
Shalom Ogunshola <shalomogunshola@gmail.com>
2021-06-25 14:36:38 +0100

    error handling

--- src/shared/filter.exception.ts
+++ src/shared/filter.exception.ts
@@ -6,10 +6,17 @@ import {
 } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 
-@Catch(HttpException)
+@Catch()
 export class RpcValidationFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
-    console.log('error filter grabs', exception.getResponse());
+  catch(exception: any, host: ArgumentsHost) {
+    if (exception.response.data) {
+      console.log('filter error response is ', exception.response.data);
+      return new RpcException({
+        ...exception.response.data,
+        statusCode: exception.response.status,
+      });
+    }
+    console.log('error filter on card service grabs', exception.getResponse());
     return new RpcException(exception.getResponse());
   }
 }
